669d7dc2cb094c9b60dd35b805c873b1eab13978
Sivet <simon@glashoff.dk>
2021-03-11 14:08:58 +0100

    InitCommit

---
+++ AssetPublisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssetPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ImageService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
